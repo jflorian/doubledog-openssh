3fe3309517a64ff9d198f9f201f9edadeab60fe8
John Florian <jflorian@doubledog.org>
2012-02-05 15:11:50 -0500

    Refactor - replace openssh-server module with openssh::server

---
+++ manifests/classes/server.pp
@@ -0,0 +1,65 @@
+# modules/openssh/manifests/classes/server.pp
+#
+# Synopsis:
+#       Configures a host as a OpenSSH server.
+#
+# Parameters:
+#       Name__________  Default_______  Description___________________________
+#
+#       NONE
+#
+# Requires:
+#       NONE
+#
+# Example usage:
+#
+#       include openssh::server
+
+class openssh::server {
+
+    include lokkit
+
+    package { 'openssh-server':
+        ensure  => installed,
+    }
+
+    if $operatingsystem == 'Fedora' and $operatingsystemrelease >= 14 {
+        $sshd_config = 'sshd_config.f14+'
+    } else {
+        $sshd_config = 'sshd_config'
+    }
+
+    file { '/etc/ssh/sshd_config':
+        group   => 'root',
+        mode    => '0600',
+        owner   => 'root',
+        require => Package['openssh-server'],
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'etc_t',
+        source  => [
+            "puppet:///private-host/openssh/${sshd_config}",
+            "puppet:///private-domain/openssh/${sshd_config}",
+            "puppet:///modules/openssh/${sshd_config}",
+        ],
+    }
+
+    lokkit::tcp_port { 'sshd':
+        port    => '22',
+    }
+
+    service { 'sshd':
+        enable          => true,
+        ensure          => running,
+        hasrestart      => true,
+        hasstatus       => true,
+        require         => [
+            Exec['open-sshd-tcp-port'],
+            Package['openssh-server'],
+        ],
+        subscribe       => [
+            File['/etc/ssh/sshd_config'],
+        ],
+    }
+
+}
